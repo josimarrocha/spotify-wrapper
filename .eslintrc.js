c9ecb0c5850645a4db1e0062de5096581d9ecfaf
Josimar <rocha_josimar@yahoo.com.br>
2019-05-29 17:02:39 +0000

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    //   node: true,
+    browser: true
+    //   es6: true,
+  },
+  extends: 'airbnb-base',
+  rules: {
+    "linebreak-style": 0,
+    'no-use-before-define': ["error", { "variables": false }]
+  },
+  globals: {
+    "describe": "writable",
+    "it": "writable"
+
+  }
+
+};
