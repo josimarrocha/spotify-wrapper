7a58f02ad4b6f965cc5a69b4d32b72aa345fd2ad
Josimar <rocha_josimar@yahoo.com.br>
2019-06-06 16:46:51 -0300

    Add Token api and headers

--- src/config.js
+++ src/config.js
@@ -1,2 +1,8 @@
-// eslint-disable-next-line import/prefer-default-export
+const TOKEN_API = 'BQCGFPe1_fB4uj3njPYlgqaRzbNYOcINSt-BAWZvj5QDrI3shL3BP5VwLIxDfTqwQHtgROqmbzTXKNIaXJ6M78KMssRq8Kfl_xqrWr_39XJPPCryYvdafFn2hUsmPkG4XuNljNcTEY6ohA5f20RXyivfaytYVGvf5SwqsCC2PUxi';
 export const API_URL = 'https://api.spotify.com/v1';
+
+export const HEADERS = {
+  headers: {
+    Authorization: `Bearer ${TOKEN_API}`,
+  },
+};
