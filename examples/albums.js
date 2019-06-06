10e486732430545c50f2dc8b38e574da4617dad5
Josimar <rocha_josimar@yahoo.com.br>
2019-06-06 14:08:14 -0300

    Examples spotify-wrapper

--- examples/albums.js
+++ examples/albums.js
@@ -1,6 +1,8 @@
-import { searchAlbuns } from '../src/main';
+// eslint-disable-next-line import/named
+import { searchAlbuns } from '../src/search';
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 global.fetch = require('node-fetch');
 
 const albums = searchAlbuns('Eminem');
-albums.then(data => data.albums.items.map(item => console.log(item)));
+albums.then(data => console.log(data));
