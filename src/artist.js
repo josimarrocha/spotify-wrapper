ffa15072cea0d81c70f65e27e665238d6d3659ea
Josimar <rocha_josimar@yahoo.com.br>
2019-06-06 16:49:33 -0300

    Add header param fetch

--- src/artist.js
+++ src/artist.js
@@ -1,14 +1,16 @@
-export const getArtistsAlbumId = id => fetch(`https://api.spotify.com/v1/artists/${id}/albums`)
+import { HEADERS } from './config';
+
+export const getArtistsAlbumId = id => fetch(`https://api.spotify.com/v1/artists/${id}/albums`, HEADERS)
   .then(data => data.json());
 
-export const getRelatedArtists = id => fetch(`https://api.spotify.com/v1/artists/${id}/related-artists`)
+export const getRelatedArtists = id => fetch(`https://api.spotify.com/v1/artists/${id}/related-artists`, HEADERS)
   .then(data => data.json());
 
-export const getArtistsTopTracks = id => fetch(`https://api.spotify.com/v1/artists/${id}/top-tracks`)
+export const getArtistsTopTracks = id => fetch(`https://api.spotify.com/v1/artists/${id}/top-tracks`, HEADERS)
   .then(data => data.json());
 
-export const getArtist = id => fetch(`https://api.spotify.com/v1/artists/${id}`)
+export const getArtist = id => fetch(`https://api.spotify.com/v1/artists/${id}`, HEADERS)
   .then(data => data.json());
 
-export const getArtists = id => fetch(`https://api.spotify.com/v1/artists?ids=${id}`)
+export const getArtists = id => fetch(`https://api.spotify.com/v1/artists?ids=${id}`, HEADERS)
   .then(data => data.json());
